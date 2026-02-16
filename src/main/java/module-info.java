53fbaf5c3fb27aafcf712b73d64d26b935e2c3fc
Amos25Rodrigues <amos25rodrigues@gmail.com>
2026-02-16 16:37:53 -0300

    É

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module br.com.amosrodrigues {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+
 
     opens br.com.amosrodrigues to javafx.fxml;
     exports br.com.amosrodrigues;
